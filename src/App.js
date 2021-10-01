8c40342f003231639f55ecac370ea8af942b2572
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-10-01 22:46:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [audio, setAudio] = useState(null);
+  const key = "f14c43fb64bc41f2b6818ad05c98aabc";
+
+  const handleRequest = () =>
+    setAudio(`http://api.voicerss.org/?key=${key}&hl=en-us&src=${text}`);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <h1>Text to speech</h1>
+        <div className="audio-form">
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeHolder="Type text here..."
+          />
+          <button onClick={handleRequest}>Send</button>
+        </div>
+      </div>
+      <div className="audio">
+        <iframe
+          title="yay"
+          src={audio}
+          width="300"
+          height="150"
+          frameborder="0"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
